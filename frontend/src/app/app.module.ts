9a91a5925d25e82fb7db987b74dfd8ee7fd8b34a
Junior de Oliveira <ellismarjr@hotmail.com>
2021-07-01 13:04:27 -0300

    feat: user can create a new product

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,20 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/header/header.component';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { ProductsCreateComponent } from './components/products/products-create/products-create.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -41,7 +44,10 @@ import { HttpClientModule } from '@angular/common/http';
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/products/products-create/products-create.component.ts
+++ frontend/src/app/components/products/products-create/products-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/model/product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products-create',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsCreateComponent implements OnInit {
 
-  constructor() { }
+  product: Product = {
+    name: 'Iphone X',
+    price: 4500
+  };
+
+  constructor(private productService: ProductsService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  createProduct() {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado com sucesso!');
+      this.router.navigate(['/products']);
+    });
+  }
+
+  cancelProduct() {
+    this.router.navigate(['/products']);
+  }
 }
