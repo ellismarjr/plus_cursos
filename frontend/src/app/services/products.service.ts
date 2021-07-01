82a407311d90f0d7295ad235c52ed10c6b77cb3b
Junior de Oliveira <ellismarjr@hotmail.com>
2021-07-01 12:27:22 -0300

    feat: add create a new product function

---
+++ frontend/src/app/services/products.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { Product } from '../model/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  baseURL = 'http://localhost:3333/products'
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
+
+  showMessage(msg: string) {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top'
+    });
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseURL, product);
+  }
+}
