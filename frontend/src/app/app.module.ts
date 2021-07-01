27053a7198cc4a948a6614bc86f825dec71db4d1
Junior de Oliveira <ellismarjr@hotmail.com>
2021-07-01 11:32:51 -0300

    chore: navigate to products create

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,12 +10,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductsCreateComponent } from './components/products/products-create/products-create.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
-import { ProductsComponent } from './pages/products/products.component';
-import { ProductsCreateComponent } from './components/products/products-create/products-create.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { ProductsCreateComponent } from './components/products/products-create/p
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
