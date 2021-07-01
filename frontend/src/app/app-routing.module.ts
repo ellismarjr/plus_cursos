896b286726691985554cbd16e2abcdcf51653a67
Junior de Oliveira <ellismarjr@hotmail.com>
2021-07-01 10:52:22 -0300

    chore: add route to product create component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsCreateComponent } from './components/products/products-create/products-create.component';
 
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
@@ -8,6 +9,7 @@ import { ProductsComponent } from './pages/products/products.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'products/create', component: ProductsCreateComponent },
 ];
 
 @NgModule({
